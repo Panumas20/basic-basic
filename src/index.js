8c4adc4a8e50465ad9182b2a106c30d001d38914
Panumas20 <74150623+Panumas20@users.noreply.github.com>
2021-11-08 23:50:15 +0700

    test react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent from './component/HelloComponent';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Function Component
+// function HelloComponent() {
+// 	return <h1>สวัสดี Component</h1>;
+// }
+
+// Class Component
+// class HelloComponent extends React.Component {
+// 	render() {
+// 		return <h1>สวัสดี Component2</h1>;
+// 	}
+// }
+
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
