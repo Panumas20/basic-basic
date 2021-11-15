7987f06dc5bc836d55fccb5a961e513d1f900b98
Panumas20 <74150623+Panumas20@users.noreply.github.com>
2021-11-15 15:39:21 +0700

    Update Code
    Update Code from  Macbook Air M1
    metallic gray

--- src/component/Transaction.js
+++ src/component/Transaction.js
@@ -1,17 +1,13 @@
 import Item from './item';
-import './Transaction.css';
-import { v4 as uuidv4 } from 'uuid';
-const Transaction = () => {
-	const data = [
-		{ title: 'ค่ายา', amount: 20000 },
-		{ title: 'เงินเดือน', amount: 2000 },
-		{ title: 'ค่างวดรถ', amount: 8000 },
-		{ title: 'ค่าน้ำมัน', amount: 1000 },
-	];
+import './Transaction.css'; 
+const Transaction = (props) => {
+	console.log(props);
+	const {items} = props 
+	
 	return (
 		<ul className="item-list">
-			{data.map((element) => {
-				return <Item {...element} key={uuidv4()} />; //ใช้ ... กรณีชื่อ properties ใน array data เหมือนใน properties ใน component Item  เช่น
+			{items.map((element) => {
+				return <Item {...element} key={element.id} />; //ใช้ ... กรณีชื่อ properties ใน array data เหมือนใน properties ใน component Item  เช่น
 				//<Item {title={element.title} amount={element.amount} }/>
 			})}
 		</ul>
