0d0ece707602cfa680f633ff46204498f40282a8
Panumas20 <74150623+Panumas20@users.noreply.github.com>
2021-11-17 01:12:51 +0700

    Update Code

--- src/component/item.js
+++ src/component/item.js
@@ -1,20 +1,23 @@
-import PropTypes from "prop-types";
-import './item.css'; 
+import PropTypes from 'prop-types';
+import './item.css';
 const Item = (props) => {
 	const { title, amount } = props;
- 
-	const status = amount < 0 ? "expense" : "income";
-	const symbol = amount < 0 ? "-" : "+";
+
+	const status = amount < 0 ? 'expense' : 'income';
+	const symbol = amount < 0 ? '-' : '+';
 	return (
 		<li className={status}>
 			{title}
-			<span>{symbol}{Math.abs(amount)} test</span>
+			<span>
+				{symbol}
+				{Math.abs(amount)}
+			</span>
 		</li>
 	);
 };
 Item.propTypes = {
 	title: PropTypes.string.isRequired,
 	amount: PropTypes.number.isRequired,
-  };
+};
 
 export default Item;
