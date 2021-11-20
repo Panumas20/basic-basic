d9197fdfe729086d08d3321e7f048a917c868dc7
Panumas20 <74150623+Panumas20@users.noreply.github.com>
2021-11-20 15:17:54 +0700

    Update Code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
+
 import Transaction from './component/Transaction';
 import FormComponent from './component/FormComponent';
-
+import DataContext from './Data/DataContext';
+import ReportComponent from './component/ReportComponent';
 
 const Title = () => <h1 style={{ color: 'red', textAlign: 'center', fontSize: '1.5rem' }}> โปรแกรมบัญชีรายรับ - รายจ่าย</h1>;
 
 function App() {
 	const initData = [];
-	const [items,setItems] = useState(initData)
-	const onAddnewItem =(newItem) =>{
-		setItems((prevItem)=>{
-			return([newItem,...prevItem])
-		})
-		console.log(`เพิ่มข้อมูล ${newItem}`)
-	}
+	const [items, setItems] = useState(initData);
+	const onAddnewItem = (newItem) => {
+		setItems((prevItem) => {
+			return [newItem, ...prevItem];
+		});
+		console.log(`เพิ่มข้อมูล ${newItem}`);
+	};
 	return (
-		<div className="container">
-			<Title/>
-			<FormComponent onAdditem={onAddnewItem}/>
-			<Transaction  items={items}/>
-		</div>
+		<DataContext.Provider
+			value={{
+				income: 50000,
+				expense: -8000,
+			}}>
+			<div className="container">
+				<Title />
+				<ReportComponent />
+				<FormComponent onAdditem={onAddnewItem} />
+				<Transaction items={items} />
+			</div>
+		</DataContext.Provider>
 	);
 }
 
--- src/component/Transaction.js
+++ src/component/Transaction.js
@@ -1,16 +1,23 @@
+import { useContext } from 'react/cjs/react.development';
+import DataContext from '../Data/DataContext';
 import Item from './item';
-import './Transaction.css'; 
+import './Transaction.css';
+
 const Transaction = (props) => {
-	console.log(props);
-	const {items} = props 
-	
+	const { income, expense } = useContext(DataContext);
+	const { items } = props;
+
 	return (
-		<ul className="item-list">
-			{items.map((element) => {
-				return <Item {...element} key={element.id} />; //ใช้ ... กรณีชื่อ properties ใน array data เหมือนใน properties ใน component Item  เช่น
-				//<Item {title={element.title} amount={element.amount} }/>
-			})}
-		</ul>
+		<div>
+			<ul className="item-list">
+				{items.map((element) => {
+					return <Item {...element} key={element.id} />; //ใช้ ... กรณีชื่อ properties ใน array data เหมือนใน properties ใน component Item  เช่น
+					//<Item {title={element.title} amount={element.amount} }/>
+				})}
+			</ul>
+			<p>รายรับ : {income}</p>
+			<p> รายจ่าย : {expense}</p>
+		</div>
 	);
 };
 export default Transaction;
